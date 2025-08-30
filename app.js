e81be7fa1ed9e93151ed96e7624322f8bf75cffb
Andres <juanandcastillo27@gmail.com>
2025-08-30 16:40:45 -0500

    Funcion Amigos

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let arregloPersonas = [];
+let arregloAmigos = [];
 
+console.log(arregloAmigos);
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value
+    if (amigo == ''){
+        alert("Porfavor, ingrese un valor");
+    }
+    else{
+        arregloAmigos.push(amigo);
+    }
+    console.log(arregloAmigos);
+    console.log(arregloAmigos.length);
+    document.querySelector('#amigo').value = '';
+}
